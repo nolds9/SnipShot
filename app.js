ab05dea3b393c0ad25938d3e860338f6a7bf5589
Nick Olds <nolds9@yahoo.com>
2016-05-06 09:28:34 -0400

    can take screenshots of every code snippet

---
+++ app.js
@@ -0,0 +1,31 @@
+var page = new WebPage(), address, output, size
+address = "https://github.com/ga-wdi-lessons/activerecord-intro"
+page.open(address, function (status) {
+  // no error checking
+  console.log(status);
+  setTimeout(function () {
+    var clipRects = page.evaluate(function () {
+      return [].map.call(document.querySelectorAll(".highlight"), function (d) {
+        return d.getBoundingClientRect()
+      })
+    });
+
+    for (var i = 0; i < clipRects.length; i++) {
+      console.log(clipRects[i], i);
+      try {
+        var clip = clipRects[i]
+        page.clipRect = {
+          top: clip.top,
+          left: clip.left,
+          width: clip.width,
+          height: clip.height
+        }
+        page.render("output" + i + ".png")
+      } catch (e) {
+        console.log(e);
+        console.log(e.stack);
+      }
+    }
+    phantom.exit()
+  }, 500)
+})
